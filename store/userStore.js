6aec75c228d9e92d0b387ecdce2a69111ebf4e00
BryanCaldeira <bryancaldeira22@gmail.com>
2024-06-09 11:07:42 -0700

    chore: add zustand store for users

---
+++ store/userStore.js
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+const useUserStore = create((set) => ({
+  user: {
+    name: "",
+    email: "",
+    token: ""
+  },
+  setUser: (user) => set((state) => ({ user: { ...state.user, ...user } })),
+  clearUser: () => set({ user: { name: "", email: "", token: "" } }),
+}));
+
+export default useUserStore;
