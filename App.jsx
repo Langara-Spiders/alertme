1508bf751315e1174d6c701c2096865b5cf7c3bf
BryanCaldeira <bryancaldeira22@gmail.com>
2024-06-08 19:07:02 -0700

    chore: add react-intl, configs for lang support

--- App.jsx
+++ App.jsx
@@ -3,15 +3,27 @@ import * as React from "react";
 import { SafeAreaView } from "react-native";
 import { MainNavigator } from "./navigation";
 import { configLight, configDark } from "./config/gluestack-ui.config";
+import { IntlProvider } from "react-intl";
+import {
+  en,
+  fr
+} from "./lang";
 
+const messages = {
+  en,
+  fr
+}
 
 export default function App() {
+  const [locale, setLocale] = React.useState("en");
 
   return (
-    <SafeAreaView style={{ flex:1 }}>
-      <StyledProvider config={configDark}>
-        <MainNavigator />
-      </StyledProvider>
-    </SafeAreaView>
+    <IntlProvider messages={messages[locale]} locale={locale} defaultLocale="en">
+      <SafeAreaView style={{ flex:1 }}>
+        <StyledProvider config={configDark}>
+          <MainNavigator />
+        </StyledProvider>
+      </SafeAreaView>
+    </IntlProvider>
   );
 }
