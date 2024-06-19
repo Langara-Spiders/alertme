e0679030a540920391c9aae3477f99e26bc52937
Karthik2506 <karthikrhaja@gmail.com>
2024-06-18 21:12:49 -0700

    fix: congrats and upvotemodal added

--- components/molecules/modals/CongratsModal.jsx
+++ components/molecules/modals/CongratsModal.jsx
@@ -1,78 +1,91 @@
-import React from 'react';
-
-import { StyleSheet } from 'react-native';
-import Button1 from '../../atoms/buttons/Button';
-import { Text, View } from '@gluestack-ui/themed';
+import { Text, View } from "@gluestack-ui/themed";
+import { StyleSheet } from "react-native";
+import Button from "../../atoms";
 
 const CongratsModal = (props) => {
-    return (
-        <View>
-             <View style={styles.confirmationCard}>
-            <Text style={styles.confirmationText}>Congratulation🎉</Text>
-            <Text style={styles.genuineText}>Your registration is complete. You are now ready to use AlertMe application</Text>
+  return (
+    <View>
+      <View style={styles.confirmationCard}>
+        <Text style={styles.confirmationText}>
+          <FormattedMessage
+            id="congratsModal.congrats"
+            defaultMessage="Congratulations🎉"
+          />
+        </Text>
+        <Text style={styles.confirmationText}>
+          <FormattedMessage
+            id="congratsModal.ready"
+            defaultMessage="Your registration is complete. You are now ready to use AlertMe application"
+          />
+        </Text>
 
-            <View style={styles.confirmationButtons}>
-                <Button1 style={styles.yes} textStyle={styles.buttonTextBlack} onPress={props.onConfirm}>Launch App🚀</Button1>
-            </View>
+        <View style={styles.confirmationButtons}>
+          <Button
+            style={styles.yes}
+            textStyle={styles.buttonTextBlack}
+            onPress={props.onConfirm}
+          >
+            <FormattedMessage
+              id="congratsModal.Launch"
+              defaultMessage="Launch App🚀"
+            />
+          </Button>
         </View>
-        </View>
-    );
-}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    confirmationCard: {
-        display: 'flex',
-        width: 346,
-        padding: 64,
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        gap: 10,
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: [{ translateX: -173 }, { translateY: -150 }],
-        backgroundColor: '#212121',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-    },
-    confirmationText: {
-        fontSize: 24,
-        marginBottom: 10,
-        color: '#FFF'
-    },
-    genuineText: {
-        fontSize: 18,
-        marginBottom: 10,
-        color: '#FFF'
-
-    },
-    confirmationButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-    },
-    confirmationButton: {
-        flex: 1,
-        padding: 10,
-        backgroundColor: '#ddd',
-        marginRight: 10,
-        alignItems: 'center',
-    },
-    yes: {
-        
-        borderRadius: 90,
-        backgroundColor: '#FFF',
-
-    },
-    buttonTextBlack: {
-        color: '#000',
-        fontSize: 18
-
-    },
-})
+  confirmationCard: {
+    display: "flex",
+    width: 346,
+    padding: 64,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: 10,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -173 }, { translateY: -150 }],
+    backgroundColor: "#212121",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  confirmationText: {
+    fontSize: 24,
+    marginBottom: 10,
+    color: "#FFF",
+  },
+  genuineText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "#FFF",
+  },
+  confirmationButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  confirmationButton: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#ddd",
+    marginRight: 10,
+    alignItems: "center",
+  },
+  yes: {
+    borderRadius: 90,
+    backgroundColor: "#FFF",
+  },
+  buttonTextBlack: {
+    color: "#000",
+    fontSize: 18,
+  },
+});
 
 export default CongratsModal;
--- components/molecules/modals/UpVoteModal.jsx
+++ components/molecules/modals/UpVoteModal.jsx
@@ -1,85 +1,91 @@
-import React from 'react';
-
-import { StyleSheet } from 'react-native';
-import Button1 from '../../atoms/buttons/Button';
-import { Text, View } from '@gluestack-ui/themed';
-
+import { Text, View } from "@gluestack-ui/themed";
+import { StyleSheet } from "react-native";
+import Button from "../../atoms";
 
 const UpVoteModal = (props) => {
-    return (
-        <View style={styles.confirmationCard}>
-            <Text style={styles.confirmationText}>👍Confirm Up-vote?</Text>
-            <Text style={styles.genuineText}>We trust that your up-vote is genuine and based on the incident you reviewed. Please refrain from misusing this community application.</Text>
+  return (
+    <View style={styles.confirmationCard}>
+      <Text style={styles.confirmationText}>
+        <FormattedMessage
+          id="congratsModal.upvote"
+          defaultMessage="👍Confirm Up-vote?"
+        />
+      </Text>
 
-            <View style={styles.confirmationButtons}>
-                <Button1 style={styles.no}>No</Button1>
-                <Button1 style={styles.yes} textStyle={styles.buttonTextBlack}>Confirm</Button1>
-            </View>
-        </View>
-    );
-}
+      <Text style={styles.confirmationText}>
+        <FormattedMessage
+          id="congratsModal.trust"
+          defaultMessage="We trust that your up-vote is genuine and based on the incident you reviewed. Please refrain from misusing this community application."
+        />
+      </Text>
+      <View style={styles.confirmationButtons}>
+        <Button style={styles.no} onPress={props.onCancel}>
+          <FormattedMessage id="congratsModal.no" defaultMessage="No" />
+        </Button>
+        <Button style={styles.yes} textStyle={styles.buttonTextBlack}>
+          <FormattedMessage id="congratsModal.yes" defaultMessage="Confirm" />
+        </Button>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    confirmationCard: {
-        display: 'flex',
-        width: 346,
-        padding: 64,
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        gap: 10,
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: [{ translateX: -173 }, { translateY: -150 }],
-        backgroundColor: '#212121',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-    },
-    confirmationText: {
-        fontSize: 24,
-        marginBottom: 10,
-        color: '#FFF'
-    },
-    genuineText: {
-        fontSize: 18,
-        marginBottom: 10,
-        color: '#FFF'
-
-    },
-    confirmationButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-    },
-    confirmationButton: {
-        flex: 1,
-        padding: 10,
-        backgroundColor: '#ddd',
-        marginRight: 10,
-        alignItems: 'center',
-    },
-    no:{
-        borderRadius: 90,
-       borderColor: '#FFF',
-       backgroundColor: '#212121',
-       borderWidth: 1,
-       borderColor: "#FFFFFF",
-
-    },
-    yes: {
-        
-        borderRadius: 90,
-        backgroundColor: '#FFF',
-
-    },
-    buttonTextBlack: {
-        color: '#000',
-    },
-})
+  confirmationCard: {
+    display: "flex",
+    width: 346,
+    padding: 64,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: 10,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -173 }, { translateY: -150 }],
+    backgroundColor: "#212121",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  confirmationText: {
+    fontSize: 24,
+    marginBottom: 10,
+    color: "#FFF",
+  },
+  genuineText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "#FFF",
+  },
+  confirmationButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  confirmationButton: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#ddd",
+    marginRight: 10,
+    alignItems: "center",
+  },
+  no: {
+    borderRadius: 90,
+    borderColor: "#FFF",
+    backgroundColor: "#212121",
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+  },
+  yes: {
+    borderRadius: 90,
+    backgroundColor: "#FFF",
+  },
+  buttonTextBlack: {
+    color: "#000",
+  },
+});
 
 export default UpVoteModal;
