bc390e3c742245dc10d5ee6b2f05822baf618a38
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-07-09 21:24:25 -0700

    feat: added 50 badges

--- pages/IssuesReportedAwards.jsx
+++ pages/IssuesReportedAwards.jsx
@@ -14,6 +14,99 @@ const A7 = require("../assets/badges/A7.png");
 const A8 = require("../assets/badges/A8.png");
 const A9 = require("../assets/badges/A9.png");
 const A10 = require("../assets/badges/A10.png");
+const A11 = require("../assets/badges/A11.png");
+const A12 = require("../assets/badges/A12.png");
+const A13 = require("../assets/badges/A13.png");
+const A14 = require("../assets/badges/A14.png");
+const A15 = require("../assets/badges/A15.png");
+const A16 = require("../assets/badges/A16.png");
+const A17 = require("../assets/badges/A17.png");
+const A18 = require("../assets/badges/A18.png");
+const A19 = require("../assets/badges/A19.png");
+const A20 = require("../assets/badges/A20.png");
+const A21 = require("../assets/badges/A21.png");
+const A22 = require("../assets/badges/A22.png");
+const A23 = require("../assets/badges/A23.png");
+const A24 = require("../assets/badges/A24.png");
+const A25 = require("../assets/badges/A25.png");
+const A26 = require("../assets/badges/A26.png");
+const A27 = require("../assets/badges/A27.png");
+const A28 = require("../assets/badges/A28.png");
+const A29 = require("../assets/badges/A29.png");
+const A30 = require("../assets/badges/A30.png");
+const A31 = require("../assets/badges/A31.png");
+const A32 = require("../assets/badges/A32.png");
+const A33 = require("../assets/badges/A33.png");
+const A34 = require("../assets/badges/A34.png");
+const A35 = require("../assets/badges/A35.png");
+const A36 = require("../assets/badges/A36.png");
+const A37 = require("../assets/badges/A37.png");
+const A38 = require("../assets/badges/A38.png");
+const A39 = require("../assets/badges/A39.png");
+const A40 = require("../assets/badges/A40.png");
+const A41 = require("../assets/badges/A41.png");
+const A42 = require("../assets/badges/A42.png");
+const A43 = require("../assets/badges/A43.png");
+const A44 = require("../assets/badges/A44.png");
+const A45 = require("../assets/badges/A45.png");
+const A46 = require("../assets/badges/A46.png");
+const A47 = require("../assets/badges/A47.png");
+const A48 = require("../assets/badges/A48.png");
+const A49 = require("../assets/badges/A49.png");
+const A50 = require("../assets/badges/A50.png");
+
+const activeBadges = [
+  A1,
+  A2,
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A8,
+  A9,
+  A10,
+  A11,
+  A12,
+  A13,
+  A14,
+  A15,
+  A16,
+  A17,
+  A18,
+  A19,
+  A20,
+  A21,
+  A22,
+  A23,
+  A24,
+  A25,
+  A26,
+  A27,
+  A28,
+  A29,
+  A30,
+  A31,
+  A32,
+  A33,
+  A34,
+  A35,
+  A36,
+  A37,
+  A38,
+  A39,
+  A40,
+  A41,
+  A42,
+  A43,
+  A44,
+  A45,
+  A46,
+  A47,
+  A48,
+  A49,
+  A50,
+];
 
 // Import all inactive badges
 const Inactive1 = require("../assets/badges/In-active 1.png");
@@ -26,8 +119,46 @@ const Inactive7 = require("../assets/badges/In-active 7.png");
 const Inactive8 = require("../assets/badges/In-active 8.png");
 const Inactive9 = require("../assets/badges/In-active 9.png");
 const Inactive10 = require("../assets/badges/In-active 10.png");
-
-const activeBadges = [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10];
+const Inactive11 = require("../assets/badges/In-active 11.png");
+const Inactive12 = require("../assets/badges/In-active 12.png");
+const Inactive13 = require("../assets/badges/In-active 13.png");
+const Inactive14 = require("../assets/badges/In-active 14.png");
+const Inactive15 = require("../assets/badges/In-active 15.png");
+const Inactive16 = require("../assets/badges/In-active 16.png");
+const Inactive17 = require("../assets/badges/In-active 17.png");
+const Inactive18 = require("../assets/badges/In-active 18.png");
+const Inactive19 = require("../assets/badges/In-active 19.png");
+const Inactive20 = require("../assets/badges/In-active 20.png");
+const Inactive21 = require("../assets/badges/In-active 21.png");
+const Inactive22 = require("../assets/badges/In-active 22.png");
+const Inactive23 = require("../assets/badges/In-active 23.png");
+const Inactive24 = require("../assets/badges/In-active 24.png");
+const Inactive25 = require("../assets/badges/In-active 25.png");
+const Inactive26 = require("../assets/badges/In-active 26.png");
+const Inactive27 = require("../assets/badges/In-active 27.png");
+const Inactive28 = require("../assets/badges/In-active 28.png");
+const Inactive29 = require("../assets/badges/In-active 29.png");
+const Inactive30 = require("../assets/badges/In-active 30.png");
+const Inactive31 = require("../assets/badges/In-active 31.png");
+const Inactive32 = require("../assets/badges/In-active 32.png");
+const Inactive33 = require("../assets/badges/In-active 33.png");
+const Inactive34 = require("../assets/badges/In-active 34.png");
+const Inactive35 = require("../assets/badges/In-active 35.png");
+const Inactive36 = require("../assets/badges/In-active 36.png");
+const Inactive37 = require("../assets/badges/In-active 37.png");
+const Inactive38 = require("../assets/badges/In-active 38.png");
+const Inactive39 = require("../assets/badges/In-active 39.png");
+const Inactive40 = require("../assets/badges/In-active 40.png");
+const Inactive41 = require("../assets/badges/In-active 41.png");
+const Inactive42 = require("../assets/badges/In-active 42.png");
+const Inactive43 = require("../assets/badges/In-active 43.png");
+const Inactive44 = require("../assets/badges/In-active 44.png");
+const Inactive45 = require("../assets/badges/In-active 45.png");
+const Inactive46 = require("../assets/badges/In-active 46.png");
+const Inactive47 = require("../assets/badges/In-active 47.png");
+const Inactive48 = require("../assets/badges/In-active 48.png");
+const Inactive49 = require("../assets/badges/In-active 49.png");
+const Inactive50 = require("../assets/badges/In-active 50.png");
 
 const inactiveBadges = [
   Inactive1,
@@ -40,7 +171,49 @@ const inactiveBadges = [
   Inactive8,
   Inactive9,
   Inactive10,
+  Inactive11,
+  Inactive12,
+  Inactive13,
+  Inactive14,
+  Inactive15,
+  Inactive16,
+  Inactive17,
+  Inactive18,
+  Inactive19,
+  Inactive20,
+  Inactive21,
+  Inactive22,
+  Inactive23,
+  Inactive24,
+  Inactive25,
+  Inactive26,
+  Inactive27,
+  Inactive28,
+  Inactive29,
+  Inactive30,
+  Inactive31,
+  Inactive32,
+  Inactive33,
+  Inactive34,
+  Inactive35,
+  Inactive36,
+  Inactive37,
+  Inactive38,
+  Inactive39,
+  Inactive40,
+  Inactive41,
+  Inactive42,
+  Inactive43,
+  Inactive44,
+  Inactive45,
+  Inactive46,
+  Inactive47,
+  Inactive48,
+  Inactive49,
+  Inactive50,
 ];
+
+export { activeBadges, inactiveBadges };
 
 const IssuesReportedAwards = () => {
   const navigation = useNavigation();
@@ -97,7 +270,7 @@ const IssuesReportedAwards = () => {
           time.
         </Text>
         <View style={styles.badgesContainer}>
-          {Array.from({ length: 10 }).map((_, index) => renderBadge(index))}
+          {Array.from({ length: 49 }).map((_, index) => renderBadge(index))}
         </View>
       </View>
     </ScrollView>
--- pages/Rewards.jsx
+++ pages/Rewards.jsx
@@ -4,9 +4,57 @@ import React, { useEffect, useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { getReward } from "../api/user";
-import badge1 from "../assets/badges/badge1.png";
-import badge2 from "../assets/badges/badge2.png";
-import badge3 from "../assets/badges/badge3.png";
+const A1 = require("../assets/badges/A1.png");
+const A2 = require("../assets/badges/A2.png");
+const A3 = require("../assets/badges/A3.png");
+const A4 = require("../assets/badges/A4.png");
+const A5 = require("../assets/badges/A5.png");
+const A6 = require("../assets/badges/A6.png");
+const A7 = require("../assets/badges/A7.png");
+const A8 = require("../assets/badges/A8.png");
+const A9 = require("../assets/badges/A9.png");
+const A10 = require("../assets/badges/A10.png");
+const A11 = require("../assets/badges/A11.png");
+const A12 = require("../assets/badges/A12.png");
+const A13 = require("../assets/badges/A13.png");
+const A14 = require("../assets/badges/A14.png");
+const A15 = require("../assets/badges/A15.png");
+const A16 = require("../assets/badges/A16.png");
+const A17 = require("../assets/badges/A17.png");
+const A18 = require("../assets/badges/A18.png");
+const A19 = require("../assets/badges/A19.png");
+const A20 = require("../assets/badges/A20.png");
+const A21 = require("../assets/badges/A21.png");
+const A22 = require("../assets/badges/A22.png");
+const A23 = require("../assets/badges/A23.png");
+const A24 = require("../assets/badges/A24.png");
+const A25 = require("../assets/badges/A25.png");
+const A26 = require("../assets/badges/A26.png");
+const A27 = require("../assets/badges/A27.png");
+const A28 = require("../assets/badges/A28.png");
+const A29 = require("../assets/badges/A29.png");
+const A30 = require("../assets/badges/A30.png");
+const A31 = require("../assets/badges/A31.png");
+const A32 = require("../assets/badges/A32.png");
+const A33 = require("../assets/badges/A33.png");
+const A34 = require("../assets/badges/A34.png");
+const A35 = require("../assets/badges/A35.png");
+const A36 = require("../assets/badges/A36.png");
+const A37 = require("../assets/badges/A37.png");
+const A38 = require("../assets/badges/A38.png");
+const A39 = require("../assets/badges/A39.png");
+const A40 = require("../assets/badges/A40.png");
+const A41 = require("../assets/badges/A41.png");
+const A42 = require("../assets/badges/A42.png");
+const A43 = require("../assets/badges/A43.png");
+const A44 = require("../assets/badges/A44.png");
+const A45 = require("../assets/badges/A45.png");
+const A46 = require("../assets/badges/A46.png");
+const A47 = require("../assets/badges/A47.png");
+const A48 = require("../assets/badges/A48.png");
+const A49 = require("../assets/badges/A49.png");
+const A50 = require("../assets/badges/A50.png");
+
 import ABCD from "../assets/images/sample_user.png";
 import {
   LeaderBoardCard,
@@ -17,10 +65,56 @@ import { routes } from "../constants";
 // Add additional badge imports as needed
 
 const badges = {
-  1: badge1,
-  2: badge2,
-  3: badge3,
-  // Add additional badge mappings as needed
+  1: A1,
+  2: A2,
+  3: A3,
+  4: A4,
+  5: A5,
+  6: A6,
+  7: A7,
+  8: A8,
+  9: A9,
+  10: A10,
+  11: A11,
+  12: A12,
+  13: A13,
+  14: A14,
+  15: A15,
+  16: A16,
+  17: A17,
+  18: A18,
+  19: A19,
+  20: A20,
+  21: A21,
+  22: A22,
+  23: A23,
+  24: A24,
+  25: A25,
+  26: A26,
+  27: A27,
+  28: A28,
+  29: A29,
+  30: A30,
+  31: A31,
+  32: A32,
+  33: A33,
+  34: A34,
+  35: A35,
+  36: A36,
+  37: A37,
+  38: A38,
+  39: A39,
+  40: A40,
+  41: A41,
+  42: A42,
+  43: A43,
+  44: A44,
+  45: A45,
+  46: A46,
+  47: A47,
+  48: A48,
+  49: A49,
+  50: A50,
 };
 
 const Rewards = (props) => {
