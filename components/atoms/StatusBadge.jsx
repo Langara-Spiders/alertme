35c7d5c76c010aaff7543f48b6d29f429c7cf213
Sandeep Guggila <sandeepguggilla99@gmail.com>
2024-07-11 01:51:16 -0700

    Added styles for status badge

--- components/atoms/StatusBadge.jsx
+++ components/atoms/StatusBadge.jsx
@@ -1,39 +1,44 @@
 import { Text, View } from "@gluestack-ui/themed";
-
 import { StyleSheet } from "react-native";
 
 const StatusBadge = (props) => {
   let backgroundColor, textColor;
 
-  if (props.status === "active" || props.status === "ACTIVE") {
+  if (props.status.toLowerCase() === "active") {
     backgroundColor = "#FECACA";
     textColor = "#7E1E1C";
-  } else if (props.status === "resolved" || props.status === "RESOLVED") {
+  } else if (props.status.toLowerCase() === "resolved") {
     backgroundColor = "#A7F1A3";
     textColor = "#185215";
-  } else if (props.status === "fixing" || props.status === "FIXING") {
+  } else if (props.status.toLowerCase() === "fixing") {
     backgroundColor = "#FFBF40";
     textColor = "#462F00";
-  } else if (props.status === "pending" || props.status === "PENDING") {
+  } else if (props.status.toLowerCase() === "pending") {
     backgroundColor = "#FFBF40";
     textColor = "#0B0C0C";
-  } else if (props.status === "rejected" || props.status === "REJECTED") {
+  } else if (props.status.toLowerCase() === "rejected") {
     backgroundColor = "gray";
     textColor = "#0B0C0C";
   }
 
   const styles = StyleSheet.create({
     container: {
+      display: "flex",
+      paddingHorizontal: 11,
+      paddingVertical: 2,
+      alignItems: "center",
+      gap: 4,
       alignSelf: "flex-start",
       backgroundColor,
       borderRadius: 20,
-      paddingHorizontal: 10,
-      paddingVertical: 2,
     },
     text: {
       color: textColor,
+      fontFamily: "Public Sans",
       fontSize: 12,
-      fontWeight: "bold",
+      fontStyle: "normal",
+      fontWeight: "600",
+      lineHeight: 15.6, // 130% of 12px
     },
   });
 
