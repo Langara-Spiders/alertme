b457782c8196c28702a458a9f14ad41829fe9b34
sim0511 <simrandeep2012@gmail.com>
2024-07-12 03:22:27 -0700

    style: 223 | login page

--- components/atoms/buttons/IconButton.jsx
+++ components/atoms/buttons/IconButton.jsx
@@ -2,6 +2,8 @@ import {
   Button as ButtonGS,
   ButtonIcon as ButtonIconGS,
   ButtonText,
+  Image,
+  View,
 } from "@gluestack-ui/themed";
 
 import { StyleSheet } from "react-native";
@@ -15,20 +17,46 @@ const IconButton = (props) => {
       action={props.action ?? "primary"}
       isDisabled={props.isDisabled ?? false}
       isFocusVisible={props.isFocusVisible ?? false}
-      style={props.style ?? iconButtonDefaultStyle.button}
+      style={styles.button}
       icon={props.icon}
+      onPress={props.onPress}
     >
-      <ButtonIconGS as={props.icon}></ButtonIconGS>
-      <ButtonText>{props.children}</ButtonText>
+      {props.icon && (
+        <ButtonIconGS
+          as={() => <Image source={props.icon} style={styles.icon} />}
+        />
+      )}
+
+      <View style={styles.buttonTextContainer}>
+        <ButtonText style={styles.text}>{props.children}</ButtonText>
+      </View>
     </ButtonGS>
   );
 };
 
 export default IconButton;
 
-const iconButtonDefaultStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   button: {
     borderRadius: 50,
-    bg: "theme.colors.pink100",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 56,
+    backgroundColor: "#131314",
+  },
+  icon: {
+    width: 28,
+    height: 28,
+  },
+  buttonTextContainer: {
+    display: "flex",
+    margin: "auto",
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+    lineHeight: 24,
+    fontWeight: "semibold",
   },
 });
