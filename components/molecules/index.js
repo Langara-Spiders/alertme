28ff3d48703dd9167248aa5e645bea3392bb5f98
sim0511 <simrandeep2012@gmail.com>
2024-07-12 04:27:52 -0700

    style: 241 | style nearby incident card

--- components/molecules/index.js
+++ components/molecules/index.js
@@ -4,6 +4,7 @@ export { default as GreatWorkCard } from "./cards/GreatWorkCard";
 export { default as IncidentCard } from "./cards/IncidentCard";
 export { default as LeaderBoardCard } from "./cards/LeaderBoardCard";
 export { default as LoginAsCard } from "./cards/LoginAsCard";
+export { default as NearbyIncidentCard } from "./cards/NearbyIncidentCard";
 export { default as NotificationCard } from "./cards/NotificationCard";
 export { default as NumOfIssuesCard } from "./cards/NumOfIssuesCard";
 export { default as PostedByCard } from "./cards/PostedByCard";
