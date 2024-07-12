eaab2c1baa763425d0df6b22eb0b1c2de8ca97df
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-07-12 01:13:29 -0700

    leaderbaord styling

--- components/molecules/cards/TopThreeCard.jsx
+++ components/molecules/cards/TopThreeCard.jsx
@@ -10,6 +10,8 @@ import { StyleSheet } from "react-native";
 import SvgUri from "react-native-svg-uri";
 
 const TopThreeCard = ({ rank, name, level, avatar, banner }) => {
+  const firstName = name.split(" ")[0]; // Extract the first name
+
   return (
     <View style={styles.container}>
       <View style={styles.avatarContainer}>
@@ -28,14 +30,14 @@ const TopThreeCard = ({ rank, name, level, avatar, banner }) => {
             />
           ) : (
             <AvatarFallbackText style={styles.avatarFallbackText}>
-              {name.charAt(0)}
+              {firstName.charAt(0)}
             </AvatarFallbackText>
           )}
         </Avatar>
         {banner && <SvgUri source={banner} style={styles.banner} />}
       </View>
       <Text style={styles.name} numberOfLines={1}>
-        {name}
+        {firstName}
       </Text>
       <Text style={styles.level}>Level {level}</Text>
     </View>
@@ -48,8 +50,8 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     marginHorizontal: -10,
-    marginRight: 1,
-    marginLeft: 1,
+    marginRight: 20,
+    marginLeft: 20,
   },
   avatarContainer: {
     position: "relative",
@@ -100,97 +102,3 @@ const styles = StyleSheet.create({
     marginTop: 2,
   },
 });
-
-// import {
-//   Avatar,
-//   AvatarFallbackText,
-//   AvatarImage,
-//   Text,
-//   View,
-// } from "@gluestack-ui/themed";
-// import React from "react";
-// import { useIntl } from "react-intl";
-// import { StyleSheet } from "react-native";
-
-// const TopThreeCard = ({ rank, name, level, avatar }) => {
-//   const intl = useIntl();
-//   const rankStyles = {
-//     1: styles.firstPlace,
-//     2: styles.secondPlace,
-//     3: styles.thirdPlace,
-//   };
-
-//   const rankText = {
-//     1: "1st",
-//     2: "2nd",
-//     3: "3rd",
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Avatar style={styles.avatar}>
-//         {avatar ? (
-//           <AvatarImage
-//             source={avatar}
-//             style={styles.avatarImage}
-//             alt="top 3 board"
-//           />
-//         ) : (
-//           <AvatarFallbackText style={styles.avatarFallbackText}>
-//             {name.charAt(0)}
-//           </AvatarFallbackText>
-//         )}
-//       </Avatar>
-//       <Text style={[styles.rankText, rankStyles[rank]]}>{rankText[rank]}</Text>
-//       <Text style={styles.name}>{name}</Text>
-//       <Text style={styles.level}>Level {level}</Text>
-//     </View>
-//   );
-// };
-
-// export default TopThreeCard;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     alignItems: "center",
-//     marginHorizontal: 10,
-//   },
-//   avatar: {
-//     width: 60,
-//     height: 60,
-//     borderRadius: 30,
-//     marginBottom: 5,
-//   },
-//   avatarImage: {
-//     width: "100%",
-//     height: "100%",
-//   },
-//   avatarFallbackText: {
-//     color: "#FFF",
-//     fontWeight: "bold",
-//     fontSize: 20,
-//   },
-//   rankText: {
-//     fontSize: 16,
-//     fontWeight: "bold",
-//     marginBottom: 5,
-//   },
-//   firstPlace: {
-//     color: "#FFD700",
-//   },
-//   secondPlace: {
-//     color: "#C0C0C0",
-//   },
-//   thirdPlace: {
-//     color: "#CD7F32",
-//   },
-//   name: {
-//     fontSize: 16,
-//     fontWeight: "bold",
-//     color: "#1E1E1E",
-//   },
-//   level: {
-//     fontSize: 14,
-//     color: "#1E1E1E",
-//   },
-// });
